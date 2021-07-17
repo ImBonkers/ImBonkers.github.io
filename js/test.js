a057ec1c835d994b02812a7d3cb2a9c9dadc81a1
samme135 <samuel_linden@hotmail.com>
2021-07-17 15:15:28 +0200

    title to select2

--- js/test.js
+++ js/test.js
@@ -1,30 +1,19 @@
 
 $(document).ready(function(){
-    h = document.getElementById("Title").innerHTML = "League Calculator"
-
-    var body = document.body
-    var div = document.createElement("div")
-    div.id = "selector_id"
-    body.appendChild(div)
-    div.append(document.createElement("img"))
+    // div.append(document.createElement("img"))
 
     $.getJSON("https://ddragon.leagueoflegends.com/api/versions.json", function(lol_version){
         $.getJSON("https://ddragon.leagueoflegends.com/cdn/"+lol_version[0]+"/data/en_US/champion.json", function(champion_data) {
-            var select = document.createElement("select")
+            var select = document.getElementById("ChampionSelect")
             select.className = "champion_selector"
             select.name = "state"
 
             $.each(champion_data["data"], function() {
                 addItemToSelect(select, this.id)
             });
-
-            var div = document.getElementById("selector_id")
-            div.appendChild(select)
             
             $('.champion_selector').select2();
             $(".champion_selector").change(function(){
-                document.getElementById("Title").innerHTML = this.value
-                document.getElementById("Title")
             });
         });
     });
