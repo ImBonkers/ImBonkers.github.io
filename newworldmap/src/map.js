2183378d552561ed9bb29bf4494cc0ac80e14051
samme135 <samuel_linden@hotmail.com>
2021-10-12 11:50:05 +0200

    Map is now scalable to any dimension and ratio

--- newworldmap/src/map.js
+++ newworldmap/src/map.js
@@ -7,17 +7,28 @@ const SECTION_COUNT = 4;
 
 $(document).ready(function()
 {
+    var canvas = document.getElementById("map-canvas");
+
     sections = {2:{},3:{},4:{},5:{},6:{},7:{},8:{}}
-    var offset = {x:0.3, y:0.3};
+    var offset = {x:0 - (get_canvas_ratio(canvas) - 1)/2, y:0};
+    var canvas_ratio = get_canvas_ratio(canvas)
     var mouse_dt = {x:0, y:0}
     var mouse_pos = {x:0, y:0}
     var mouse_down = false;
 
-    var zoom = 3
-    var canvas = document.getElementById("map-canvas");
+    var zoom = 2
     
-    window.addEventListener("resize", function()
+    window.addEventListener("resize", function(event)
     {
+        tmp = get_canvas_ratio(canvas);
+        delta_r = tmp - canvas_ratio;
+        canvas_ratio = tmp;
+        canvas_rect = tmp;
+        console.log(delta_r)
+        offset = {
+            x:offset.x - (delta_r/(2*get_zoom_scale(zoom))),
+            y:offset.y
+        }
     });
 
     canvas.addEventListener("mousewheel", function(event)
@@ -49,11 +60,6 @@ $(document).ready(function()
                 x: relative_offset.x - 1/(2*zoom_power) - (2*mouse_offset.x + (relative_offset.x - offset.x)),
                 y: relative_offset.y - 1/(2*zoom_power) - (2*mouse_offset.y + (relative_offset.y - offset.y))
             };
-        }
-        else if (zoom_delta == 0){
-            if (zoom == 2){
-                offset = {x:0,y:0}
-            }
         }
 ;
         event.preventDefault();
@@ -105,9 +111,16 @@ function get_mouse_pos(canvas, evt){
     var rect = canvas.getBoundingClientRect();
 
     return {
-        x: (evt.clientX - rect.left) / rect.width,
+        x: (evt.clientX - rect.left) / rect.height,
         y: (evt.clientY - rect.top) / rect.height
     };
+}
+
+function get_canvas_ratio(canvas){
+    var rect = canvas.getBoundingClientRect();
+    var ratio = (rect.right - rect.left) / (rect.bottom - rect.top);
+    return ratio;
+
 }
 
 function get_url(x, y, zoom){
@@ -156,16 +169,16 @@ function draw_offset(offset, zoom, canvas){
     );
 }
 
-function draw_image(x, y, offset, canvas, img){
-    var ratio = canvas.width / canvas.height
+function draw_image(x, y, canvas, img){;
+    var ratio = get_canvas_ratio(canvas);
     section_width = canvas.width / SECTION_COUNT
 
     canvas.getContext("2d").drawImage(
         img, 0, 0, img.width, img.height, 
-        section_width*x,
+        section_width*x/ratio,
         section_width*y,
-        section_width, 
-        section_width/ratio);
+        section_width/ratio, 
+        section_width);
 }
 
 function clamp_region(region, boundary){
@@ -199,14 +212,14 @@ async function images_loaded(region, zoom){
 }
 
 function update_map(canvas, zoom, offset){
-    canvas.width =  1000;
-    canvas.height = 1000;
+    canvas.width =  2000;
+    canvas.height = 2000;
 
     var boundary = {lower: 0, upper: ZOOM_SECTIONS[zoom]};
     var region = {
         lower_x:boundary.lower + Math.floor(offset.x * ZOOM_SECTIONS[zoom]),
         lower_y:boundary.lower + Math.floor(offset.y * ZOOM_SECTIONS[zoom]),
-        upper_x:boundary.lower + Math.ceil(offset.x * ZOOM_SECTIONS[zoom]) + SECTION_COUNT,
+        upper_x:boundary.lower + Math.ceil(offset.x * ZOOM_SECTIONS[zoom]) + SECTION_COUNT*get_canvas_ratio(canvas),
         upper_y:boundary.lower + Math.ceil(offset.y * ZOOM_SECTIONS[zoom]) + SECTION_COUNT,
     };
 
@@ -219,13 +232,13 @@ function update_map(canvas, zoom, offset){
                 if (!sections[zoom][[x,y]]){
                     load_image(get_url(x, y, zoom)).then(img => {
                         sections[zoom][[x, y]] = img;
-                        draw_image(x-offset.x*Math.pow(2, zoom), y-offset.y*Math.pow(2, zoom), offset, canvas, img);
+                        draw_image(x-offset.x*Math.pow(2, zoom), y-offset.y*Math.pow(2, zoom), canvas, img);
                         // draw_offset(offset, zoom, canvas);
                         // draw_section(x, y, zoom, canvas, offset);
                     });
                 }
                 else{
-                    draw_image(x-offset.x*Math.pow(2, zoom), y-offset.y*Math.pow(2, zoom), offset, canvas, sections[zoom][[x, y]]);
+                    draw_image(x-offset.x*Math.pow(2, zoom), y-offset.y*Math.pow(2, zoom), canvas, sections[zoom][[x, y]]);
                 }
             }
         }
